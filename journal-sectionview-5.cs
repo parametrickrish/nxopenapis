32f81f42148d9afbc39feeb53a3598e9e0105bc0
parametrickrish <connect@designautomationlife.com>
2025-05-24 18:56:09 +0530

    modified

--- journal-sectionview-5.cs
+++ journal-sectionview-5.cs
@@ -1,6 +1,7 @@
 using System;
 using NXOpen;
 using NXOpen.Drawings;
+using static NXOpen.CAM.FBM.ThreadFeatureGeometry;
 
 public class NXJournal
 {
@@ -36,15 +37,23 @@ public class NXJournal
 
         sectionViewBuilder1.ParentView.View.Value = baseView1;
 
-        //NXOpen.Point point1 = (NXOpen.Point)workPart.Points.FindObject("ENTITY 2 9");
-        //NXOpen.Point3d point2 = new NXOpen.Point3d(81.5, 61.305707157111343, 10.0);
-        //sectionViewBuilder1.ViewPlacement.AlignmentPoint.SetValue(point1, baseView1, point2);
+        NXOpen.Drawings.DraftingBodyCollection draftingBodyColl = baseView1.DraftingBodies;
 
-        //NXOpen.Point3d vieworigin1 = new NXOpen.Point3d(-81.5, -47.0, 45.5);
-        //sectionViewBuilder1.ViewStyle.ViewStylePerspective.ViewOrigin = vieworigin1;
+        NXOpen.Drawings.DraftingBody draftingbBody = null;
 
-        NXOpen.Drawings.DraftingBody draftingBody1 = (NXOpen.Drawings.DraftingBody)baseView1.DraftingBodies.FindObject("0 EXTRUDE(2)  0");
-        NXOpen.Drawings.DraftingCurve draftingCurve1 = (NXOpen.Drawings.DraftingCurve)draftingBody1.DraftingCurves.FindObject("(Extracted Edge) EDGE * 120 * 200 {(74.339364107334,73.7082923377101,10)(95.8212717853319,61.3057071571113,10)(74.339364107334,48.9031219765126,10) EXTRUDE(2)}");
+        foreach (DraftingBody draftingbBody1 in draftingBodyColl)
+        {
+            draftingbBody = draftingbBody1;
+        }
+
+        foreach (NXOpen.Drawings.DraftingCurve draftingCurve in draftingbBody.DraftingCurves)
+        {
+            //Assignment 
+            // check if the found drafting curve is circle, if circle then assign the object to variable draftingCurve1 and continue
+        }
+
+        //NXOpen.Drawings.DraftingBody draftingBody1 = (NXOpen.Drawings.DraftingBody)baseView1.DraftingBodies.FindObject("0 EXTRUDE(2)  0");
+        NXOpen.Drawings.DraftingCurve draftingCurve1 = (NXOpen.Drawings.DraftingCurve)draftingbBody.DraftingCurves.FindObject("(Extracted Edge) EDGE * 120 * 200 {(74.339364107334,73.7082923377101,10)(95.8212717853319,61.3057071571113,10)(74.339364107334,48.9031219765126,10) EXTRUDE(2)}");
         NXOpen.Point point3;
         point3 = workPart.Points.CreatePoint(draftingCurve1, NXOpen.SmartObject.UpdateOption.AfterModeling);
 
