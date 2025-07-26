2f99cc4a7aca7e0e1cc376e009de9149650a2e50
parametrickrish <connect@designautomationlife.com>
2025-07-26 13:36:25 +0530

    firstcommit

---
+++ 1. create_blank_part_with_drawing_sheet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NXOpen;
+public class create_blank_part_with_drawing_sheet
+{
+    public static void Main(string[] args)
+    {
+        Session theSession = Session.GetSession();
+        FileNew fileNew1;
+        fileNew1 = theSession.Parts.FileNew();
+        fileNew1.TemplateFileName = "Blank";
+        fileNew1.Application = FileNewApplication.Gateway;
+        fileNew1.Units = NXOpen.Part.Units.Millimeters;
+        fileNew1.NewFileName = @"C:\temp\test.prt";
+        fileNew1.UseBlankTemplate = true;
+        fileNew1.MakeDisplayedPart = true;
+        NXObject nXObject1 = fileNew1.Commit();
+        fileNew1.Destroy();
+        Part workPart = theSession.Parts.Work;
+        Part displayPart = theSession.Parts.Display;
+        NXOpen.Drawings.DrawingSheet nullDrawings_DrawingSheet = null;
+        NXOpen.Drawings.DrawingSheetBuilder drawingSheetBuilder1;
+        drawingSheetBuilder1 =
+        workPart.DrawingSheets.DrawingSheetBuilder(nullDrawings_DrawingSheet);
+        drawingSheetBuilder1.Option =
+        NXOpen.Drawings.DrawingSheetBuilder.SheetOption.StandardSize;
+        drawingSheetBuilder1.Units =
+        NXOpen.Drawings.DrawingSheetBuilder.SheetUnits.Metric;
+        drawingSheetBuilder1.StandardMetricScale =
+        NXOpen.Drawings.DrawingSheetBuilder.SheetStandardMetricScale.S11;
+        drawingSheetBuilder1.Height = 210.0;
+        drawingSheetBuilder1.Length = 297.0;
+        drawingSheetBuilder1.ProjectionAngle =
+        NXOpen.Drawings.DrawingSheetBuilder.SheetProjectionAngle.First;
+        NXObject nXObject2 = drawingSheetBuilder1.Commit();
+        drawingSheetBuilder1.Destroy();
+        // Application Switch will be performed AFTER program ends, see Docs.
+        UI.GetUI().MenuBarManager.ApplicationSwitchRequest("UG_APP_DRAFTING")
+        ;
+        // Part is saved but Application is NOT Drafting yet!
+        displayPart.Save(BasePart.SaveComponents.False,
+        BasePart.CloseAfterSave.False);
+    }
+    public static int GetUnloadOption(string dummy)
+    {
+        return
+    (int)Session.LibraryUnloadOption.Immediately;
+    }
+}
---
+++ 2. update_all_solid_edge_components.vb
@@ -0,0 +1,52 @@
+﻿Option Strict Off
+Imports System
+Imports NXOpen
+Imports NXOpen.UF
+Imports NXOpen.Assemblies
+
+Module update_all_solid_edge_components
+    Dim s As Session = Session.GetSession()
+    Dim ufs As UFSession = UFSession.GetUFSession()
+    Dim lw As ListingWindow = s.ListingWindow
+    Sub Main()
+        lw.Open()
+        Dim root As Component =
+        s.Parts.Display.ComponentAssembly.RootComponent
+        If Not root Is Nothing Then
+            lw.WriteLine(root.DisplayName())
+            reportChildrensBodyStatus(root, 1)
+        Else
+            lw.WriteLine("The displayed part is not an assembly.")
+        End If
+    End Sub
+    Sub reportChildrensBodyStatus(ByVal comp As Component, ByVal indent As Integer)
+        Dim space As String = Nothing
+        For ii As Integer = 1 To indent
+            space = space & " "
+        Next
+        For Each child As Component In comp.GetChildren()
+            Try
+                Dim cPart As Part = child.Prototype
+                Dim num_bodies_before = cPart.Bodies.ToArray().Length
+                ufs.Modl.EditImportBodyFeature(cPart.Tag,
+                UFModl.ImportBodyFeatureEditOption.ImportBodyFeatureUpdateLink,
+                Nothing)
+                Dim num_bodies_after = cPart.Bodies.ToArray().Length
+                If num_bodies_before <> num_bodies_after Then
+                    Dim num_bodies_loaded = num_bodies_after -
+                    num_bodies_before
+
+                    lw.WriteLine(space & child.DisplayName() &
+                    " was updated with " &
+                    num_bodies_loaded.ToString() & " bodies.")
+                End If
+            Catch ex As Exception
+                lw.WriteLine("Error: " & ex.Message)
+            End Try
+            reportChildrensBodyStatus(child, indent + 1)
+        Next
+    End Sub
+    Public Function GetUnloadOption(ByVal dummy As String) As Integer
+        Return Session.LibraryUnloadOption.Immediately
+    End Function
+End Module
