951669f702ec5fc20f41ddbec3dc76bef1e4b1b7
parametrickrish <connect@designautomationlife.com>
2026-02-11 21:40:07 +0530

    tabular-note

--- Object Orientated Programming/Abstraction.cs
+++ Object Orientated Programming/Abstraction.cs
@@ -1,66 +1,54 @@
+using Microsoft.VisualBasic;
 using System;
-using NXOpen;
-using NXOpen.Features;
-
-public class GeometryHelper
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using System.Runtime.InteropServices;
+namespace EnableContourSelection_CSharp.csproj
 {
-    private Session _session;
-    private Part _workPart;
-
-    public GeometryHelper()
+    partial class SolidWorksMacro
     {
-        _session = Session.GetSession();
-        // It's safer to fetch the work part every time the helper is initialized
-        _workPart = _session.Parts.Work;
+
+        ModelDoc2 swModel;
+        object vSkLines;
+        bool boolstatus;
+
+        public void Main()
+        {
+            
+            string parttemplate = "";
+            parttemplate = swApp.GetUserPreferenceStringValue((int)swUserPreferenceStringValue_e.swDefaultTemplatePart);
+            swModel = (ModelDoc2)swApp.NewDocument(parttemplate, 0, 0, 0);
+            //swModel = (ModelDoc2)swApp.NewDocument("C:\\ProgramData\\SOLIDWORKS\\SOLIDWORKS 2014\ emplates\\Part.prtdot", 0, 0, 0);
+            swModel = (ModelDoc2)swApp.ActiveDoc;
+            boolstatus = swModel.SetUserPreferenceToggle((int)swUserPreferenceToggle_e.swDisplayOrigins, true);
+            swModel.ClearSelection2(true);
+
+            vSkLines = swModel.SketchManager.CreateCornerRectangle(-0.0390769010920735, 0.0405984975017191, 0, 0.0129020232818107, -0.0166534302871355, 0);
+            swModel.ClearSelection2(true);
+            vSkLines = swModel.SketchManager.CreateCornerRectangle(-0.00751826843645631, 0.0156092594749566, 0, 0.0487922329685375, -0.041704950991857, 0);
+            swModel.ClearSelection2(true);
+            swModel.SketchManager.InsertSketch(true);
+
+            // Enable contour selection
+            ((SelectionMgr)(swModel.SelectionManager)).EnableContourSelection = true;
+            // Select a contour to extrude
+            swModel.Extension.SelectByID2("Sketch1", "SKETCHREGION", 0, 0.01, 0, true, 4, null, 0);
+            swModel.FeatureManager.FeatureExtrusion3(true, false, false, 0, 0, 0.01, 0.01, false, false, false,
+            false, 0, 0, false, false, false, false, true, true, true,
+            0, 0, false);
+            // Disable contour selection
+            ((SelectionMgr)(swModel.SelectionManager)).EnableContourSelection = false;
+            swModel.ClearSelection2(true);
+
+        }
+
+        public SldWorks swApp;
+
     }
 
-    public Block CreateSimpleBlock(double x, double y, double z, string length, string width, string height)
-    {
-        if (_workPart == null) return null;
-
-        // 1. Initialize the Builder
-        BlockFeatureBuilder blockBuilder = _workPart.Features.CreateBlockFeatureBuilder(null);
-
-        // 2. Set the Origin
-        Point3d origin = new Point3d(x, y, z);
-        blockBuilder.SetOrigin(origin);
-
-        // 3. Set Dimensions (NX uses strings for RHS to allow expressions)
-        blockBuilder.Length.RightHandSide = length;
-        blockBuilder.Width.RightHandSide = width;
-        blockBuilder.Height.RightHandSide = height;
-
-        // 4. Commit and Clean up
-        // Note: Commit returns an NXObject, we cast it to Feature
-        Feature blockFeature = (Feature)blockBuilder.Commit();
-        blockBuilder.Destroy();
-
-        return (Block)blockFeature;
-    }
 }
 
-// In NX, all code must live inside a class. 
-// "Main" is the entry point for the Journal.
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        GeometryHelper helper = new GeometryHelper();
-
-        try
-        {
-            // The Abstraction in action: clean, readable commands.
-            helper.CreateSimpleBlock(0, 0, 0, "100", "50", "25");
-            helper.CreateSimpleBlock(150, 0, 0, "10", "10", "10");
-        }
-        catch (Exception ex)
-        {
-            // Simple error reporting in NX
-            Session.GetSession().ListingWindow.Open();
-            Session.GetSession().ListingWindow.WriteMessage("Error: " + ex.Message);
-        }
-    }
-
-    // Required for NX Journals to tell the system how to unload the library
-    public static int GetUnloadOption(string dummy) { return (int)Session.LibraryUnloadOption.Immediately; }
-}
---
+++ Tabular-Note/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NXOpen;
+
+public class NXJournal
+{
+    public static void Main(string[] args)
+    {
+        NXOpen.Session theSession = NXOpen.Session.GetSession();
+        NXOpen.Part workPart = theSession.Parts.Work;
+        NXOpen.Part displayPart = theSession.Parts.Display;
+       
+        NXOpen.Annotations.Table table1 = ((NXOpen.Annotations.Table)workPart.Annotations.Tables.FindObject("ENTITY 165 12 1"));
+        NXOpen.DisplayableObject displayableObject1 = ((NXOpen.DisplayableObject)workPart.FindObject("ENTITY 218 12 1"));
+        table1.EditCellText(displayableObject1, "345");
+
+        // ----------------------------------------------
+        //   Menu: Tools->Automation->Journal->Stop Recording
+        // ----------------------------------------------
+
+    }
+    public static int GetUnloadOption(string dummy) { return (int)NXOpen.Session.LibraryUnloadOption.Immediately; }
+}
